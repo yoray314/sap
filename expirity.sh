9d56edce8e4ffaad3536af50db28619081b22816
yoray <yoray@localhost.localdomain>
2025-10-02 01:18:12 +0300

    feat: output the end date from certificate

--- expirity.sh
+++ expirity.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
-set -exo pipefail
+#set -x
 
-mapfile -t websites_list < $@
+#NOTE: Consider making options for the script.
 
-for website in ${websites_list}; do
-	#TODO: find a fix for the kept connection
-	openssl s_client -connect ${website}:443
+#NOTE: An assumption is made that the input file contains one website per line, as no specification was provided.
+#make a list of websites from input file
+mapfile -t websites_list < "$@"
+
+for website in "${websites_list[@]}"; do
+	#input from /dev/null is to prevent openssl from waiting for input
+	#redirecting stderr to /dev/null is to suppress connection information
+	certificate=$(openssl s_client -connect "${website}:443" < /dev/null 2>/dev/null)
+	if [ $? -ne 0 ]; then
+		echo "[FAILED] Could not connect to ${website} on port 443."
+		continue
+	fi
+	expiry_date=$(openssl x509 -noout -enddate <<< "${certificate}")
+	echo "[INFO] ${website} expires on: ${expiry_date}"
 done
